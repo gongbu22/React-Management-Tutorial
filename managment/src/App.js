7afc957ab6dd258d3e6e4d53343f49d9db2f7463
gongbu22 <gongbu22@gmail.com>
2023-01-22 18:12:17 +0900

    6장 material-ui

--- managment/src/App.js
+++ managment/src/App.js
@@ -1,6 +1,21 @@
-
 import './App.css';
 import Customer from './components/Customer'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+const styles = {
+  root: {
+    width: '100%',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+}
 
 const customers = [{
   'id': 1,
@@ -29,9 +44,22 @@ const customers = [{
 ]
 
 function App() {
+  const {classes} = this.props;
   return (
-    <div>
-      {customers.map(c => {   //반복을 위해 map 사용
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {customers.map(c => {   //반복을 위해 map 사용
         return(
           <Customer 
             key={c.id}    //map함수는 key값이 필요하다 마침 id가 숫자기때문에 그냥 id값을 주겠다.
@@ -44,8 +72,11 @@ function App() {
           />
         )
       })}
-    </div>
+        </TableBody>
+      </Table>
+      
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
